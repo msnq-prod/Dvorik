eee5ac1cb7a6edef1b832b5df4d0da23060fba62
msnq-prod <nikitamysnik030202@gmail.com>
2025-09-17 16:44:24 +1000

    made working release

--- windows/stop.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set TASK_NAME=MarmBot
-
-echo Stopping scheduled task "%TASK_NAME%" (if running)...
-schtasks /end /tn "%TASK_NAME%"
-exit /b 0
-
--- windows/uninstall_marmbot.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-rem Uninstall MarmBot Scheduled Task
-powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0uninstall_marmbot.ps1"
-pause
-
